978dd3eef64a622ac907feaee8ba91735f7f21ca
James Liu <65922029+liuj-42@users.noreply.github.com>
2022-04-15 04:21:03 -0400

    lab 8

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from './http.service';
+import { PieComponent } from './pie/pie.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit{
+  loggedin = false;
+  lab4Show = true;
+  lab5Show = false;
+  pie_1: any;
+  pie_2: any;
+  constructor(private httpService: HttpService) {}
+
+  ngOnInit(): void {
+    if (searchParams.has("error")) {
+      console.log("redirected")
+      this.loggedin = false;
+    }
+    // console.log(`1: ${this.loggedin}`)
+    // console.log("alive")
+    if (searchParams.has("logged_in")) {
+      // console.log("logged in");
+      this.loggedin = true;
+    }
+    // console.log(`2: ${this.loggedin}`)
+  }
+
+  lab4() {
+    this.lab4Show = true;
+    this.lab5Show = false;
+  }
+
+  lab5(pie1: PieComponent, pie2: PieComponent) {
+
+    this.lab4Show = false;
+    this.lab5Show = true;
+  }
+
+
 }
+
+let searchParams = new URLSearchParams(window.location.search);
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,50 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { AlertModule,AlertConfig } from 'ngx-bootstrap/alert';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { FormsModule } from '@angular/forms';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { BsDatepickerModule, BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+import { BsDropdownModule,BsDropdownConfig } from 'ngx-bootstrap/dropdown';
+import { ModalModule, BsModalService } from 'ngx-bootstrap/modal';
+import { HttpClientModule } from '@angular/common/http';
+import { LoggedinComponent } from './loggedin/loggedin.component';
+import { SongItemComponent } from './song-item/song-item.component';
+import { GraphsComponent } from './graphs/graphs.component';
+import { ScatterComponent } from './scatter/scatter.component';
+import { PieComponent } from './pie/pie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LoggedinComponent,
+    SongItemComponent,
+    GraphsComponent,
+    ScatterComponent,
+    PieComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AccordionModule,
+    AlertModule,
+    ButtonsModule,
+    FormsModule,
+    CarouselModule,
+    CollapseModule,
+    BsDatepickerModule.forRoot(),
+    BsDropdownModule,
+    ModalModule
   ],
-  providers: [],
+  providers: [BsModalService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
